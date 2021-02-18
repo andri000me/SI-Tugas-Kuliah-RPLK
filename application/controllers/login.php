254f627117dbe47e1874e2fc9ada37ada9d568d2
fika-dwi <66341500+fika-dwi@users.noreply.github.com>
2021-02-18 11:52:27 +0700

    Update login.php

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -3,39 +3,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller{
 
-	function __construct(){
-		parent::__construct();		
-		$this->load->model('m_login');
+  function __construct(){
+    parent::__construct();    
+    $this->load->model('m_login');
 
-	}
+  }
 
-	function index(){
-		$this->load->view('v_login');
-	}
+  function index(){
+    $this->load->view('v_login');
+  }
 
-	function aksi_login(){
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
-		$where = array(
-			'username' => $username,
-			'password' => md5($password)
-			);
-		$cek = $this->m_login->cek_login("user",$where)->num_rows();
-		if($cek > 0){
+  function aksi_login(){
+    $username = $this->input->post('username');
+    $password = $this->input->post('password');
+    $where = array(
+      'username' => $username,
+      'password' => sha1($password)
+      );
+    $cek = $this->m_login->cek_login("pemakai",$where)->num_rows();
+    if($cek > 0){
 
-			$data_session = array(
-				'nama' => $username,
-				'status' => "login"
-				);
+      $data_session = array(
+        'nama' => $username,
+        'status' => "login"
+        );
 
-			$this->session->set_userdata($data_session);
+      $this->session->set_userdata($data_session);
 
-		}else{
-			echo "Username dan password salah !";
-		}
-	}
+      redirect('/welcome');
 
-	function logout(){
-		$this->session->sess_destroy();
-	}
+    }else{
+      echo "Username dan password salah !";
+    }
+  }
+
+  function logout(){
+    $this->session->sess_destroy();
+    redirect('/login');
+  }
 }
